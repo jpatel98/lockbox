430f0f55d55ee927b269d9ce0b21d546ff603ca5
Jigar Patel <jpxtel98@outlook.com>
2023-03-10 19:19:04 -0500

    Divide code into header file, logic and main making it modular

--- src/lockbox.c
+++ src/lockbox.c
@@ -1,18 +1,7 @@
+#include "lockbox.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define MAX_LOGINS 100
-#define MAX_WEBSITE_LENGTH 50
-#define MAX_USERNAME_LENGTH 50
-#define MAX_PASSWORD_LENGTH 50
-
-struct Login
-{
-  char website[MAX_WEBSITE_LENGTH];
-  char username[MAX_USERNAME_LENGTH];
-  char password[MAX_PASSWORD_LENGTH];
-};
 
 int numLogins = 0;
 struct Login logins[MAX_LOGINS];
@@ -177,56 +166,4 @@ void deleteLogin()
     saveLoginsToFile();
     printf("Login deleted successfully\n");
   }
-}
-
-// Display the main menu
-int main()
-{
-  loadLoginsFromFile();
-  char password[50];
-  printf("Enter master password: ");
-  scanf("%s", password);
-
-  if (strcmp(password, "mypassword") != 0)
-  {
-    printf("Invalid password\n");
-    return 0;
-  }
-
-  int choice;
-  do
-  {
-    printf("\nMenu:\n");
-    printf("1. Add a new login\n");
-    printf("2. View saved logins\n");
-    printf("3. Update an existing login\n");
-    printf("4. Delete a saved login\n");
-    printf("5. Exit\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-
-    switch (choice)
-    {
-    case 1:
-      addLogin();
-      break;
-    case 2:
-      viewLogins();
-      break;
-    case 3:
-      updateLogin();
-      break;
-    case 4:
-      deleteLogin();
-      break;
-    case 5:
-      printf("Goodbye\n");
-      break;
-    default:
-      printf("Invalid choice\n");
-      break;
-    }
-  } while (choice != 5);
-
-  return 0;
 }
---
+++ src/main.c
@@ -0,0 +1,55 @@
+#include "lockbox.h"
+#include <stdio.h>
+#include <string.h>
+
+// Display the main menu
+int main()
+{
+  loadLoginsFromFile();
+  char password[50];
+  printf("Enter master password: ");
+  scanf("%s", password);
+
+  if (strcmp(password, "mypassword") != 0)
+  {
+    printf("Invalid password\n");
+    return 0;
+  }
+
+  int choice;
+  do
+  {
+    printf("\nMenu:\n");
+    printf("1. Add a new login\n");
+    printf("2. View saved logins\n");
+    printf("3. Update an existing login\n");
+    printf("4. Delete a saved login\n");
+    printf("5. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    
+    switch (choice)
+    {
+    case 1:
+      addLogin();
+      break;
+    case 2:
+      viewLogins();
+      break;
+    case 3:
+      updateLogin();
+      break;
+    case 4:
+      deleteLogin();
+      break;
+    case 5:
+      printf("Goodbye\n");
+      break;
+    default:
+      printf("Invalid choice\n");
+      break;
+    }
+  } while (choice != 5);
+
+  return 0;
+}
